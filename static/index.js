26034b38058b8d5efd2a27561c3a8011f474360c
leightonwelch <leighton@SocialCodes-MacBook-Pro-6.local>
2014-07-15 13:26:09 -0400

    added event handler

--- static/index.js
+++ static/index.js
@@ -1,3 +1,9 @@
+// to do: 
+// abstract template to html file
+// use underscore to manipulate JSON instea of object.keys()
+// have change_dd1 call a python function (from backbone event handler)
+// reroute all other urls, why #table
+
 var App = {
             Models: {},
             Collections: {},
@@ -12,21 +18,26 @@ App.Models.Table = Backbone.Model.extend({
 });
 
 App.Views.Table = Backbone.View.extend({
-      el: $('#container'),
-      //template: _.template("hello: <%= val %>"),
-      template: _.template("<select><% _.each(column, function(colname) { %> <option><%= colname %></option> <% }); %></select>"),
-      initialize: function(){
+    el: $('#container'),
+    //template: _.template("hello: <%= val %>"),
+    template: _.template("<select class=dd1><% _.each(column, function(colname) { %> <option><%= colname %></option> <% }); %></select></br><select><option></option></select>"),
+    initialize: function(){
         this.render();
-      },
-      render: function(){
+    },
+    render: function(){
         //console.log('success ', this.model.toJSON());
         //$(this.el).html("TEST");
         //$(this.el).html(JSON.stringify(this.model.toJSON()));
         //$(this.el).html(this.template({column: ['moe', 'curly', 'larry']}));
         $(this.el).html(this.template({column: Object.keys(this.model.toJSON())}));
-
-      }
-    });
+    },
+    events: {
+        "change .dd1": "change_dd1"
+    },
+    change_dd1: function(e) {
+        console.log(e.originalEvent.srcElement.value);
+    },
+});
 
 App.Router = Backbone.Router.extend({
     routes: {
